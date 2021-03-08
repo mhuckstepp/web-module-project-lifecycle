50afaa349c887a2c7734f4b7d0e901d425290286
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-08 14:47:35 -0800

    initial commit

---
+++ lifecylce/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> test </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
